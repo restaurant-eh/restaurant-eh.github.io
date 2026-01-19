afaa7381334bc3ec63b2bc442c80ec8e6fe8d419
restaurant-eh <erzahlendehappchen@gmail.com>
2026-01-19 23:45:38 +0100

    Initial upload
    Added all webseite files: Bilder, CSS, HTML, JS, XML

--- JS/Getraenke/erfrischungsgetraenke.js
+++ JS/Getraenke/erfrischungsgetraenke.js
@@ -8,12 +8,14 @@ fetch("/XML/Getraenke/erfrischungsgetraenke.xml")
         const kohlensaeure = xml.getElementsByTagName("kohlensaeure")[0].getElementsByTagName("getraenk")
         for (let i = 0; i < kohlensaeure.length; i++) {
             const name = kohlensaeure[i].getElementsByTagName("name")[0].textContent;
+            const inhalt = kohlensaeure[i].getElementsByTagName("inhalt")[0].textContent;
             const beschreibung = kohlensaeure[i].getElementsByTagName("beschreibung")[0].textContent;
             const preis = kohlensaeure[i].getElementsByTagName("preis")[0].textContent
             const div = document.createElement("div");
             div.classList.add("getraenk");
             div.innerHTML = `
                 <h3 class="getraenk-name">${name}</h3>
+                <p class="getraenk-inhalt">(${inhalt}l)</p>
                 <p class="getraenk-beschreibung">${beschreibung}</p>
                 <strong class="getraenk-preis">${preis} €</strong> `
             kohlensaeureContainer.appendChild(div);
@@ -22,12 +24,14 @@ fetch("/XML/Getraenke/erfrischungsgetraenke.xml")
         const fruchtsaefte = xml.getElementsByTagName("fruchtsaefte")[0].getElementsByTagName("getraenk")
         for (let i = 0; i < fruchtsaefte.length; i++) {
             const name = fruchtsaefte[i].getElementsByTagName("name")[0].textContent;
+            const inhalt = fruchtsaefte[i].getElementsByTagName("inhalt")[0].textContent;
             const beschreibung = fruchtsaefte[i].getElementsByTagName("beschreibung")[0].textContent;
             const preis = fruchtsaefte[i].getElementsByTagName("preis")[0].textContent
             const div = document.createElement("div");
             div.classList.add("getraenk");
             div.innerHTML = `
                 <h3 class="getraenk-name">${name}</h3>
+                <p class="getraenk-inhalt">(${inhalt}l)</p>
                 <p class="getraenk-beschreibung">${beschreibung}</p>
                 <strong class="getraenk-preis">${preis} €</strong> `
             fruchtsaefteContainer.appendChild(div);
